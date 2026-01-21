8d66ac1fa3757941e694ef58974b83546fe58687
nikunj-ramteke <nikunjr2204@gmail.com>
2026-01-21 23:17:50 +0530

    Create Program2.py

---
+++ Assignment_09/Program2.py
@@ -0,0 +1,14 @@
+def ChkGreater(No1,No2):
+    if No1 > No2 :
+        print(No1,"is greater")
+    else:
+        print(No2,"is greater")
+
+def main():
+    No1 = int(input("Enter first  number : "))
+    No2 = int(input("Enter first  number : "))
+    
+    ChkGreater(No1,No2)
+
+if __name__ == "__main__":
+    main()
